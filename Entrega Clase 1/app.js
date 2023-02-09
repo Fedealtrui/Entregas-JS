f5bfa045e6c8088203a45294777ca219d2728f16
Fede Altrui <82673183+Fedealtrui@users.noreply.github.com>
2023-02-09 12:17:58 -0300

    Actualizar app.js

--- Entrega Clase 1/app.js
+++ Entrega Clase 1/app.js
@@ -30,3 +30,109 @@ alert(`Gracias ${nombre} por visitarme.`)
 
 
 
+
+
+// Practica facultad pseudo codigo
+
+// ALGORITMO masVariables 
+    
+    //Definir otras variables
+
+    //DEFINIR nombre COMO CADENA;
+    //DEFINIR apellido COMO CADENA;
+
+    //ESCRIBIR "ingresa tu nombre";
+    //LEER nombre;
+
+    //ESCRIBIR "ingresa tu apellido";
+    //LEER apellido;
+
+    //ESCRIBIR "Hola, ", nombre, apellido,"."
+
+//FINALGORITMO
+
+
+//ALGORITMO variablesLogicas
+
+    //uso de booleanos;
+    //DEFINIR verdadero COMO LOGICO;
+    //DEFINIR falso COMO LOGICO;
+
+    //verdadero <- Verdadero;
+    //falso <- Falso;
+
+//FINALGORITMO
+
+
+
+//ALGORITMO edades
+
+    //DEFINIR edad COMO ENTERO;
+    //DEFINIR esMayor COMO LOGICO;
+    //ESCRIBIR "Ingresa tu edad";
+    //LEER edad;
+    
+    //esMayor <- edad>=18;
+
+    //SI (esMayor)
+        //ESCRIBIR "Bienvenido";
+    //SINO
+        //ESCRIBIR "Debes ser mayor de 18 años para ingresar";
+    //FINSI;
+
+//FINALGORITMO
+
+
+//ALGORITMO decision2
+    
+    
+    //DEFINIR edad COMO ENTERO;
+    //DEFINIR altura COMO REAL;
+    //DEFINIR esMayor COMO LOGICA;
+    //DEFINIR esAlto COMO LOGICA;
+
+
+    //ESCRIBIR "Cual es tu edad?";
+    //LEER edad;
+
+    //ESCRIBIR "Cuanto medis?";
+    //LEER altura;
+
+
+    //esMayor <- edad>=18;
+    //esAlto <- altura>=1.60;
+
+
+    //SI(esMayor y esAlto)
+        //escribir "Aprobaste!";
+    //SINO
+        //ESCRIBIR "No aprobaste :(";
+    //FINSI;
+
+//FINALGORITMO;
+
+
+
+
+//ALGORITMO decisionSimple
+
+    //DEFINIR edad COMO ENTERO
+    //DEFINIR altura COMO REAL
+
+    //Escribir "Cuantos años tenes?"
+    //LEER edad
+
+    //ESCRIBIR "Cuanto medis?"
+    //LEER altura
+
+    //SI(edad >= 18) y (altura >= 1.6)
+        //ESCRIBIR "Aprobaste! :)"
+    //SINO
+        //ESCRIBIR "No aprobaste! :("
+    //FINSI
+
+//FINALGORITMO
+
+
+
+        
